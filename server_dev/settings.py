786b8b68ad443553d5e04be23aa5edacf470757e
myeongrokkim <64912096+myeongrokkim@users.noreply.github.com>
2021-03-21 22:13:06 +0900

    add host

--- server_dev/settings.py
+++ server_dev/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'qf_q=2^cayrygjg$21=-mr5_j+9m@5e0nj==e4$lfi46zvie)6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
